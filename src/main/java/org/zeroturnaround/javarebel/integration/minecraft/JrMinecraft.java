603f10b8b244307a14a1b94ff4c1b6769bde6226
henri5 <henriviik@gmail.com>
2015-06-22 13:21:01 +0300

    added comment about expected object type

--- src/main/java/org/zeroturnaround/javarebel/integration/minecraft/JrMinecraft.java
+++ src/main/java/org/zeroturnaround/javarebel/integration/minecraft/JrMinecraft.java
@@ -4,5 +4,5 @@ import org.zeroturnaround.javarebel.integration.monitor.MonitoredResource;
 
 public interface JrMinecraft {
   @SuppressWarnings("unused")
-  void _jrMonitorResource(MonitoredResource mr, Object o);
+  void _jrMonitorResource(MonitoredResource mr, Object /*net.minecraft.client.resources.IResourcePack*/ iResourcePack);
 }
